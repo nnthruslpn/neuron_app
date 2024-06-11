144dcffdd43e23141b5585fa74599b6b809c137b
nnthruslpn <nnthruslpn@gmail.com>
2024-06-11 14:34:11 +0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,13 @@ const StyledApp = styled.div`
   background-color: #e8e8e8;
   color: black;
 
-  @media (prefers-color-scheme: dark) {
-    background-color: #222;
-    color: white;
-  }
   min-height: 100vh;
   padding: 20px 20px;
 `;
 
 const AppContainer = styled.div`
   max-width: 900px;
-  margin: 0 auto;
+  margin: 0 ;
   display: flex;
   justify-content: space-between;
 `;
