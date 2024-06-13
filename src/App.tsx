1175b358a23f14791904e0b3c59b7aebd8fe8322
nnthruslpn <nnthruslpn@gmail.com>
2024-06-13 10:25:06 +0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,55 +26,87 @@ const AppContainer = styled.div`
 `;
 
 const QuantityComponent = styled.div`
-  height: 20vh;
   display: flex;
-  justify-content: space-between;
+  height: 20vh;
   align-items: center;
-  background-color: #f7f7f7;
   padding: 16px;
   border-radius: 8px;
+  background-color: #f2f2f2;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
-const QuantityText = styled.span`
-  font-size: 18px;
-  margin-bottom: 0;
-  margin-left: 16px;
+
+const QuantityInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 16px;
+`;
+
+const QuantityTitle = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 4px;
+  white-space: nowrap; /* Добавьте это свойство */
+`;
+
+const QuantityCount = styled.span`
+  font-size: 22px;
+  font-weight: bold;
+  color: #666;
 `;
 
 const SliderContainer = styled.div`
-  width: 10vw; /* Make the slider wider */
+  width: 10vw;
   height: 20vh;
   background-color: #ddd;
   border-radius: 10px;
   margin-right: 16px;
-  display: flex; /* Add this to make the slider fill the container */
-  flex-direction: column-reverse; /* Добавить это свойство */
-  
+  display: flex;
+  flex-direction: column-reverse;
 `;
 
 const Slider = styled.div`
-  width: 100%; /* Make the blue bar fill the entire width */
-  height: 0%; /* Initialize the height to 0 */
-  background-color: #000000;
+  width: 100%;
+  height: 0%;
+   background-color: #000; /* Черный цвет для слайдера */
   border-radius: 10px;
   transition: height 0.3s ease-in-out;
-  align-self: flex-start; /* Make the slider start from the bottom */
+  align-self: flex-start;
 `;
 
+
 const NftComponent = styled.div`
-  height: 20vh;
   display: flex;
-  justify-content: space-between;
+  height: 20vh;
   align-items: center;
-  background-color: #f7f7f7;
   padding: 16px;
   border-radius: 8px;
+  background-color: #f2f2f2;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
 
-const NftText = styled.span`
+const NftIcon = styled.img`
+  width: 32px;
+  height: 32px;
+  margin-right: 16px;
+`;
+
+const NftInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const NftTitle = styled.span`
   font-size: 18px;
-  margin-bottom: 0;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 4px;
+`;
+
+const NftCount = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  color: #666;
 `;
 const FlexBoxRow = styled.div`
   display: flex;
@@ -90,18 +122,42 @@ const FlexBoxCol = styled.div`
   gap: 10px;
   height: 100vh; /* Установите высоту родительского элемента */
 `;
+
+const BalanceComponent = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  background-color: #f2f2f2;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+const BalanceLabel = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+  margin-right: 16px;
+`;
+
+const BalanceValue = styled.span`
+  font-size: 24px;
+  font-weight: bold;
+  color: #666;
+`;
 const NewComponent = styled.div`
   height: 50vh;
   width: 82vw;
   display: flex;
   flex-direction: column;
-  justify-content: space-between; /* Добавьте это свойство */
-  align-items: center; /* Добавьте это свойство */
-  background-color: #f7f7f7;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f2f2f2;
   padding: 16px;
   border-radius: 8px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 `;
+
 
 const BuyButton = styled.button`
   width: 70vw;
@@ -133,59 +189,63 @@ function App() {
   return (
     <StyledApp>
     <FlexBoxRow style={{ 
-      justifyContent: 'space-between', 
-      alignItems: 'center', 
-      padding: '8px 16px', 
-      backgroundColor: '#f7f7f7', 
-      height: 40, 
-      position: 'absolute', 
-      top: 0, 
-      left: 0, 
-      right: 0, 
-      borderBottomLeftRadius: 10, 
-      borderBottomRightRadius: 10 
-    }}>
-      <h2 style={{
-        color: '#666', // серый цвет текста
-        background: 'linear-gradient(to right, #666, #999)', // градиент для текста
-        WebkitBackgroundClip: 'text', // для корректного отображения градиента в Chrome
-        WebkitTextFillColor: 'transparent' // для корректного отображения градиента в Chrome
-      }}>
-        NEURON
-      </h2>
-      <TonConnectButton /> {/* используем компонент TonConnectButton */}
-    </FlexBoxRow>
+  justifyContent: 'space-between', 
+  alignItems: 'center', 
+  padding: '8px 16px', 
+  backgroundColor: '#f7f7f7', 
+  height: 40, 
+  position: 'absolute', 
+  top: 0, 
+  left: 0, 
+  right: 0, 
+  borderBottomLeftRadius: 10, 
+  borderBottomRightRadius: 10 
+}}>
+  <h2 style={{
+    color: '#666', // серый цвет текста
+    background: 'linear-gradient(to right, #666, #999)', // градиент для текста
+    WebkitBackgroundClip: 'text', // для корректного отображения градиента в Chrome
+    WebkitTextFillColor: 'transparent' // для корректного отображения градиента в Chrome
+  }}>
+    NEURON
+  </h2>
+  <TonConnectButton/> {/* Используем стилизованную кнопку TonConnect */}
+</FlexBoxRow>
     <AppContainer style={{ marginTop: 60, width: '90vw', marginLeft: 0, marginRight: 0 }}>
   <FlexBoxRow style={{ justifyContent: 'stretch', alignItems: 'center', width: '100%' }}>
     <FlexBoxCol style={{ flex: 1, width: '50%' }}>
-      <NftComponent>
-        <NftText>
-          NFT's: {nftCount}
-        </NftText>
-      </NftComponent>
+    <NftComponent>
+  <NftIcon src="https://img.icons8.com/?size=100&id=FgtvXcOD0APh&format=png&color=000000" alt="NFT Icon" />
+  <NftInfo>
+    <NftTitle>Your NFT Collection</NftTitle>
+    <NftCount>{nftCount} NFT's</NftCount>
+  </NftInfo>
+</NftComponent>
       
-      <NewComponent>
-      <Filler />
-      <BuyButton>Buy</BuyButton>
-     </NewComponent>
+<NewComponent>
+  <BalanceComponent>
+    <BalanceLabel>Your Balance</BalanceLabel>
+    <BalanceValue>123.45 TON</BalanceValue>
+  </BalanceComponent>
+  <Filler />
+  <BuyButton>Buy</BuyButton>
+</NewComponent>
  
     </FlexBoxCol>
     <FlexBoxCol style={{ flex: 1, width: '50%' }}>
-      <QuantityComponent>
-        <SliderContainer>
-          <Slider
-            style={{
-              height: `${(quantity / maxQuantity) * 100}%`,
-            }}
-          />
-        </SliderContainer>
-        <QuantityText>
-          Free NFT's<br/>
-          <span style={{ fontSize: 12 }}>
-            {quantity}/{maxQuantity}
-          </span>
-        </QuantityText>
-      </QuantityComponent>
+    <QuantityComponent>
+  <QuantityInfo>
+    <QuantityTitle>Free NFT's</QuantityTitle>
+    <QuantityCount>{quantity}/{maxQuantity}</QuantityCount>
+  </QuantityInfo>
+  <SliderContainer>
+    <Slider
+      style={{
+        height: `${(quantity / maxQuantity) * 100}%`,
+      }}
+    />
+  </SliderContainer>
+</QuantityComponent>
       
     </FlexBoxCol>
     
